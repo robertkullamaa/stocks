2767cf21a2cdc23328338c7d30c0740c4b8d92af
Robert Kullamaa <robert.kullamaa@elisa.ee>
2021-10-10 11:33:33 +0300

    latest price txt

--- priceApi.js
+++ priceApi.js
@@ -1,19 +1,45 @@
 const rp = require('request-promise');
+const fs = require('fs')
 const cheerio = require('cheerio');
 
-const stocks = ['IGN1L.VS', 'SAB1L.VS', 'LHV1T.TL', 'CPA1T.TL', 'MSFT'];
+//const stocks = ['IGN1L.VS', 'SAB1L.VS', 'LHV1T.TL', 'CPA1T.TL', 'MSFT', 'BTC'];
+const stocks = ['BTC-USD'];
 
 let url = 'https://finance.yahoo.com/quote/';
 
 stocks.forEach(stock => {
-	console.log(stock);
 	rp(url + stock)
   .then(function(html){
   	const $ = cheerio.load(html);
 
   	const open = $('#quote-market-notice').children()[0].children[0].data.indexOf('Market open.') > -1;
+    const price = $('#quote-market-notice').parent().children()[0].children[0].data;
+    const change = $('#quote-market-notice').parent().children()[1].children[0].data;
 
-  	console.log(stock + ' - ' + $('#quote-market-notice').parent().children()[0].children[0].data + ' ' + $('#quote-market-notice').parent().children()[1].children[0].data + ' ' + (open ? 'OPEN' : 'CLOSED'));
+    let ts = Date.now();
+
+    let date_ob = new Date(ts);
+    let date = date_ob.getDate();
+    let month = date_ob.getMonth() + 1;
+    let year = date_ob.getFullYear();
+    // current hours
+    let hours = date_ob.getHours();
+    // current minutes
+    let minutes = date_ob.getMinutes();
+    // current seconds
+    let seconds = date_ob.getSeconds();
+    const timestring = year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds;
+    
+
+    const res = timestring + ' ' + stock + ' - ' + price + ' ' + change + ' ' + (open ? 'OPEN' : 'CLOSED');
+
+  	console.log(res);
+    fs.writeFile('latest_price.txt' , res, function (err,data) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(data);
+    });
   
   })
   .catch(function(err){
