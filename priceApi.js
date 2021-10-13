af482d7d7459824a9acfbdf74eaf164f1930e988
Robert Kullamaa <robert.kullamaa@elisa.ee>
2021-10-13 21:35:40 +0300

    fixed file writing

--- priceApi.js
+++ priceApi.js
@@ -7,24 +7,25 @@ const stocks = ['IGN1L.VS', 'SAB1L.VS', 'LHV1T.TL', 'CPA1T.TL', 'MSFT'];
 
 let url = 'https://finance.yahoo.com/quote/';
 let res = '';
+let promises = [];
 stocks.forEach(stock => {
-	rp(url + stock)
-  .then(function(html){
-  	const $ = cheerio.load(html);
+	promises.push(rp(url + stock))
+});
+  
+Promise.all(promises).then(values => {
+  values.forEach((html, index) => {
+    const $ = cheerio.load(html);
 
-  	const open = $('#quote-market-notice').children()[0].children[0].data.indexOf('Market open.') > -1;
+    const open = $('#quote-market-notice').children()[0].children[0].data.indexOf('Market open.') > -1;
     const price = $('#quote-market-notice').parent().children()[0].children[0].data;
     const change = $('#quote-market-notice').parent().children()[1].children[0].data;
 
-    res += stock + ' ' + price + ' ' + change + ' ' + (open ? 'OPEN' : 'CLOSED') + '\n';
-    fs.writeFile('latest_price.txt' , res, function (err,data) {
-      if (err) {
-        return console.log(err);
-      }
-    });
-  })
-  .catch(function(err){
-    //handle error
+    res += stocks[index] + ' ' + price + ' ' + change + ' ' + (open ? 'OPEN' : 'CLOSED') + '\n';
   });
 
+  fs.writeFile('latest_price.txt' , res, function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+  });
 });
